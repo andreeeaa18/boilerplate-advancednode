aa25d76ec593236b2fcb8b7e6734be43c680a317
andreeeaa18 <andreevandreea.18@gmail.com>
2025-04-28 15:23:10 +0300

    Third try

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
-});
-
-const path = require("path");
 app.set("view engine", "pug");
+
 app.set("views", path.join(__dirname, "views/pug"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
 });
